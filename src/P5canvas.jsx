4f6e9b814f86ae3638db68560a65076fc9ab2078
Sakpal Amit <amitsakpal744@gmail.com>
2025-02-21 20:21:18 +0530

    created the canvasReference
     - implemented hook effect to keep running the canvas one time
     - created parameterd sketch and then implement setup for canvas
     - added the height and width of the canvas along with its parent being the canvasRef
       (in progress)

--- src/P5canvas.jsx
+++ src/P5canvas.jsx
@@ -1,10 +1,24 @@
 import React, { useEffect, useRef } from 'react'
+import p5 from 'p5'
 
-
+// this consist of the canvas , that the code will run in (window)
 const P5canvas = () => {
+
+  const canvasRef = useRef(null);
+
+  useEffect(()  => {
+
+    const sketch = (p) => {
+      p.setup = () => {
+        p.createCanvas(600,600).parent(canvasRef.current);
+      }
+    }
+    
+  },[])
+
   return (
     <div>
-      
+
     </div>
   )
 }
