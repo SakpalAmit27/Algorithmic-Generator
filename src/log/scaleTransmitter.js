04d297354e813e3cc7c03499dfce3b49bac6acfe
Sakpal Amit <amitsakpal744@gmail.com>
2025-02-24 15:13:16 +0530

    [fixed] the constructor braces error , along with changed currentSelectedScale
     - implemented the setScale to change , to change the Scale logically , and set the currentSelectedScale as the scaleName selected by the user while changing the scale

--- src/log/scaleTransmitter.js
+++ src/log/scaleTransmitter.js
@@ -8,11 +8,23 @@ class scaleTransmitter{
             "D Major": ["D4", "E4", "F#4", "G4", "A4", "B4", "C#5"],
             "A Minor": ["A4", "B4", "C4", "D4", "E4", "F4", "G4"],
             "G Major": ["G4", "A4", "B4", "C4", "D4", "E4", "F#4"],
-    }
+    };
 
     // @SakpalAmit27 - default selected scale will be Dmaj // 
-    this.defaultScale = "D Major"
+    this.currentSelectedScale = "D Major";
+    }
 
+    
+    // incase setting a newScale // 
+
+    setScale(scaleName){
+        if(this.scales[scaleName]){
+            this.currentSelectedScale = scaleName;
+        }
+        else{
+            console.error(`scale : ${scaleName} not found !`);
+        }
+    }
     
 
     
