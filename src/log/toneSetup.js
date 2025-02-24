ad1a2ff9f411536a7a118ea275098872b54d23d9
Sakpal Amit <amitsakpal744@gmail.com>
2025-02-24 15:46:23 +0530

    preparing to import the scaleTransmitter unto the toneSetup to implement better scales [convienience]

--- src/log/toneSetup.js
+++ src/log/toneSetup.js
@@ -2,6 +2,8 @@ import * as Tone from 'tone'
 
 import Stack from './Stack.js';
 
+import scaleTransmit from './scaleTransmitter.js';
+
 
 
 // instanced the noteStack lol // 
@@ -12,33 +14,6 @@ const noteStack = new Stack();
 const synth = new Tone.Synth().toDestination(); 
 
 
-
-export const playRandomNote = () => {
-
-        
-  const notes = ["C4", "D4", "E4", "F4", "G4", "A4", "B4"];
-
-  const randomIndex = Math.floor(Math.random() * notes.length);
-  const selectedNote = notes[randomIndex]; // f @SakpalAmit27
-  
-  
-
-
-  synth.triggerAttackRelease(selectedNote,"8n");
-
-  // noteStack.push(randomNote); 
-
-  console.log("selected note : ",selectedNote); 
-
-  return selectedNote;
-
-  
-}
-
-export const getPlayedNotes = () => {
-
-  return noteStack.getStack();
-}
 
 
 
