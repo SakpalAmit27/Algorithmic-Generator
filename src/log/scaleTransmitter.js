8e5b7d95181410dd3b5a21b77c2244e90867579f
Sakpal Amit <amitsakpal744@gmail.com>
2025-02-24 15:06:24 +0530

    created ScaleTransmitter.js , this will help sending other scale for user incase he selects something other than Cmaj or min , other scales will be added later
     (utilizing) -- classes here as its better option for this

---
+++ src/log/scaleTransmitter.js
@@ -0,0 +1,16 @@
+
+
+class scaleTransmitter{
+
+    constructor(){
+        this.scales = {
+            "C Major": ["C4", "D4", "E4", "F4", "G4", "A4", "B4"],
+            "D Major": ["D4", "E4", "F#4", "G4", "A4", "B4", "C#5"],
+            "A Minor": ["A4", "B4", "C4", "D4", "E4", "F4", "G4"],
+            "G Major": ["G4", "A4", "B4", "C4", "D4", "E4", "F#4"],
+    }
+
+    this.currentScale = "D Major";
+    
+    
+}
